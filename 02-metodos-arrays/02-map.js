29518117b1d01ceb42ca562b4f5709c464579c98
arthurduraes-mma2 <arthurduraes1@gmail.com>
2026-08-18 13:57:50 -0300

    TxT

---
+++ 02-metodos-arrays/02-map.js
@@ -0,0 +1,11 @@
+const numeros = [1, 2, 3, 4, 5];
+
+const dobrado = numeros.map((num) => num * 2);
+
+console.log(dobrado);
+
+let nomes = ["Tardin", "Pacheco", "Duraes", "Carlao"];
+
+let mensagem = nomes.map((nome) => `Olá, ${nome}`);
+
+console.log(mensagem);
